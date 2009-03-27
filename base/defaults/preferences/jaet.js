cbda8677e1382dc3e7cf25878567c1bc11267de1
Ilya Konovalov <aragaer@gmail.com>
2009-03-27 23:19:59 +0300

    Working stub

---
+++ base/defaults/preferences/jaet.js
@@ -0,0 +1,5 @@
+pref("toolkit.defaultChromeURI", "chrome://jaet/content/jaet.xul");
+pref("general.useragent.locale", "en_US");    		
+pref("browser.preferences.instantApply", true);
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
